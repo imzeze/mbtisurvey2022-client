e6a21143f2c378888b7e15abe5eac613acbc69cb
imzeze <jihyekim019@gmail.com>
2022-02-26 20:49:57 +0900

    fix: font 폴더 위치 변경

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -4,28 +4,28 @@ const GlobalStyle = css`
     @font-face {
         font-family: 'Pretendard';
         font-weight: 700;
-        src: url('assets/fonts/Pretendard-Bold.woff2') format('woff2');
+        src: url('fonts/Pretendard-Bold.woff2') format('woff2');
         font-display: fallback;
     }
 
     @font-face {
         font-family: 'Pretendard';
         font-weight: 500;
-        src: url('assets/fonts/Pretendard-Medium.woff2') format('woff2');
+        src: url('fonts/Pretendard-Medium.woff2') format('woff2');
         font-display: fallback;
     }
 
     @font-face {
         font-family: 'Pretendard';
         font-weight: 400;
-        src: url('assets/fonts/Pretendard-Regular.woff2') format('woff2');
+        src: url('fonts/Pretendard-Regular.woff2') format('woff2');
         font-display: fallback;
     }
 
     @font-face {
         font-family: 'Pretendard';
         font-weight: 300;
-        src: url('assets/fonts/Pretendard-Light.woff2') format('woff2');
+        src: url('fonts/Pretendard-Light.woff2') format('woff2');
         font-display: fallback;
     }
 
