eed74678c92b716b9c4db47dabd28e8ff67ce4e8
imzeze <jihyekim019@gmail.com>
2022-03-06 21:03:03 +0900

    feat: 휴대폰 확인 및 토큰 저장 로직 추가

--- src/components/auth/AuthContainer.tsx
+++ src/components/auth/AuthContainer.tsx
@@ -1,4 +1,6 @@
 import { useFirebaseAuth } from '../../util/usePhoneAuth';
+import Cookies from 'js-cookie';
+
 import {
     getAuth,
     RecaptchaVerifier,
@@ -6,26 +8,39 @@ import {
 } from 'firebase/auth';
 import AuthPresenter from './AuthPresenter';
 import { useEffect } from 'react';
+import api from '../../util/api';
 
 const AuthContainer = () => {
     const { app } = useFirebaseAuth();
     const auth = getAuth(app);
-    auth.languageCode = 'ko';
 
+    auth.languageCode = 'ko';
     const sendAuthCode = async (phone: string) => {
         if (!phone) return;
-        const internationalPhone = phone.slice(1);
-        signInWithPhoneNumber(
-            auth,
-            '+82' + internationalPhone,
-            window.recaptchaVerifier,
-        )
-            .then((confirmationResult) => {
-                window.confirmationResult = confirmationResult;
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        const response = await api({
+            method: 'get',
+            url: 'users/checkPhone',
+            data: {
+                phone,
+            },
+        });
+
+        if (response.status === 200) {
+            const internationalPhone = phone.slice(1);
+            signInWithPhoneNumber(
+                auth,
+                '+82' + internationalPhone,
+                window.recaptchaVerifier,
+            )
+                .then((confirmationResult) => {
+                    window.confirmationResult = confirmationResult;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        } else {
+            alert('이미 참여한 사용자 입니다.');
+        }
     };
 
     const confirmAuthCode = (data: { [x: string]: string }) => {
@@ -34,7 +49,14 @@ const AuthContainer = () => {
             .then((result: any) => {
                 alert('인증이 완료되었습니다.');
                 if (result.user.accessToken) {
-                    console.log('accessToken', result.user.accessToken);
+                    Cookies.set('auth', result.user.accessToken);
+                    api({
+                        method: 'post',
+                        url: '/tokens',
+                        data: {
+                            token: result.user.accessToken,
+                        },
+                    });
                 }
             })
             .catch((error: any) => {
