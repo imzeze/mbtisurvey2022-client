1f358db9f1e1e43bb333725d68f103219c2d648b
imzeze <jihyekim019@gmail.com>
2022-08-17 00:16:05 +0900

    feat: title 및 description 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { Layout, Template } from '../components/layout';
 import MainPresenter from '../components/main/MainPresenter';
 
 const Home: NextPage = () => {
     return (
-        <Layout showHeader={false} showFooter={false}>
-            <Template>
-                <MainPresenter />
-            </Template>
-        </Layout>
+        <div>
+            <Head>
+                <title>MBTI SURVEY 2022 : 대국민 MBTI 조사</title>
+            </Head>
+            <Layout showHeader={false} showFooter={false}>
+                <Template>
+                    <MainPresenter />
+                </Template>
+            </Layout>
+        </div>
     );
 };
 
