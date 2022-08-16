c7f54c1a1fc29f0343ef03c8326be4b53dedcc0f
imzeze <jihyekim019@gmail.com>
2022-08-17 00:00:04 +0900

    feat: favicon 추가 및 home 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,14 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Router from 'next/router';
-import { useEffect } from 'react';
+import { Layout, Template } from '../components/layout';
+import MainPresenter from '../components/main/MainPresenter';
 
 const Home: NextPage = () => {
-    useEffect(() => {
-        Router.push('/main');
-    }, []);
-
     return (
-        <div>
-            <Head>
-                <title>MBTI SURVEY 2022 : 대국민 MBTI 조사</title>
-                <meta
-                    name="description"
-                    content="우리가 아는 MBTI에 대한 편견은 사실일까요? 여러분의
-                소중한 시간을 투자해주세요!"
-                />
-                <meta property="og:image" content="/ogimage.png" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        </div>
+        <Layout showHeader={false} showFooter={false}>
+            <Template>
+                <MainPresenter />
+            </Template>
+        </Layout>
     );
 };
 
